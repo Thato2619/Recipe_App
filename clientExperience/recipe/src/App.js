414af45ef855731898856987908fe404c08dbf2c
Thato2619 <108408682+Thato2619@users.noreply.github.com>
2023-02-27 19:10:37 +0200

    create a components folder that contains the a Footer and Navbar component
    - add router in the home page:
     - let it contain navbar, home and footer component

--- clientExperience/recipe/src/App.js
+++ clientExperience/recipe/src/App.js
@@ -9,29 +9,44 @@ import Home from './websitePages/Home';
 import Login from './websitePages/Login';
 import Write from './websitePages/Write';
 import Single from './websitePages/Single';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>This is God's world!</div>,
+    element: <div>
+      <Navbar />
+      <Home />
+      <Footer />
+    </div>,
   },
 
    {
-    path: "/",
-    element: <div>This is God's world!</div>,
+    path: "/register",
+    element: <Register />,
   },
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+
   {
     path: "/",
     element: <div>This is God's world!</div>,
   },
+
   {
     path: "/",
     element: <div>This is God's world!</div>,
   },
+
   {
     path: "/",
     element: <div>This is God's world!</div>,
   },
+
 ]);
 
 function App() {
